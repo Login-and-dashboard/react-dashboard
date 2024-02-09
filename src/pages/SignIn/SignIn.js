eb380e283425342f74963b67b807d45ac678b7ae
unknown <ngobao2001@gmail.com>
2024-02-09 21:34:33 +0700

    Update IP

--- src/pages/SignIn/SignIn.js
+++ src/pages/SignIn/SignIn.js
@@ -44,7 +44,7 @@ const Login = () => {
       redirect: 'follow'
     };
 
-    await fetch("http://3.236.246.13:8443/v1/account/updatenft", requestOptions);
+    await fetch("http://3.234.74.113:8443/v1/account/updatenft", requestOptions);
     setLoggedIn(true);
   }, [usableNfts, accessToken, setLoggedIn])
 
